33749e55aef45705092e237e3626872f186f88d7
Sagnik Dam <sagnikdam29@gmail.com>
2026-02-07 12:27:44 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -59,5 +59,7 @@ def history():
 # ---------------- RUN SERVER ---------------- #
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render compatibility
-    app.run(host="0.0.0.0", port=port, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
